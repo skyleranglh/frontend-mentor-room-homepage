83348008222ca8765b8b78f8bd2574d8e3d1aa46
Skyler Ang <spikegreen2@gmail.com>
2025-07-12 02:12:47 +0800

    Fix url paths

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,20 @@
 const slides = [
   {
-    imageMobile: "../assets/images/mobile-image-hero-1.jpg",
-    imageDesktop: "../assets/images/desktop-image-hero-1.jpg",
+    imageMobile: "assets/images/mobile-image-hero-1.jpg",
+    imageDesktop: "assets/images/desktop-image-hero-1.jpg",
     heading: "Discover innovative ways to decorate",
     description:
       "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
   },
   {
-    imageMobile: "../assets/images/mobile-image-hero-2.jpg",
-    imageDesktop: "../assets/images/desktop-image-hero-2.jpg",
+    imageMobile: "assets/images/mobile-image-hero-2.jpg",
+    imageDesktop: "assets/images/desktop-image-hero-2.jpg",
     heading: "We are available all across the globe",
     description: `With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.`,
   },
   {
-    imageMobile: "../assets/images/mobile-image-hero-3.jpg",
-    imageDesktop: "../assets/images/desktop-image-hero-3.jpg",
+    imageMobile: "assets/images/mobile-image-hero-3.jpg",
+    imageDesktop: "assets/images/desktop-image-hero-3.jpg",
     heading: "Manufactured with the best materials",
     description: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.`,
   },
